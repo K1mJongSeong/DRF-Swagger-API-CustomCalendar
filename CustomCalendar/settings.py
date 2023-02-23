0bd3a50b19110fd8452c60999af85088bab0d101
[K1mJongSeong] <miaer456@naver.com>
2023-02-23 10:40:49 +0900

    Swagger API

--- CustomCalendar/settings.py
+++ CustomCalendar/settings.py
@@ -39,6 +39,8 @@ INSTALLED_APPS = [
     'django.contrib.messages',
     'django.contrib.staticfiles',
     'rest_framework',
+    'rest_framework_swagger',
+    'drf_yasg',
     'app.apps.AppConfig',
 ]
 
--- CustomCalendar/urls.py
+++ CustomCalendar/urls.py
@@ -1,13 +1,28 @@
 from django.contrib import admin
 from django.urls import path ,include
+from drf_yasg.views import get_schema_view
+from drf_yasg import openapi
+from rest_framework.permissions import AllowAny
 from app import views
 from app.views import NansuList, NansuUrlDetail
 from app.views import OrderList, OrderInfoList, CalendarList
 
+schema_view = get_schema_view(
+    openapi.Info(
+        title="Open API",
+        default_version='v1',
+        description="시스템 API",
+        terms_of_service="https://www.google.com/policies/terms/",
+    ),
+    public=True,
+    permission_classes=(AllowAny,),
+)
 
-
-
+app_name='blog'
 urlpatterns = [
+    path('swagger<str:format>', schema_view.without_ui(cache_timeout=0), name='schema-json'),
+    path('swagger/', schema_view.with_ui('swagger', cache_timeout=0), name='schema-swagger-ui'),
+    path('redoc/', schema_view.with_ui('redoc', cache_timeout=0), name='schema-redoc'),
     path('admin/', admin.site.urls), #localhost:8000/admin/ 경로입니다. admin.site.urls 함수가 호출됩니다.
     path('nansu/', views.nansu, name='nansu'), 
     path('',views.index),#root url을 의미합니다. views.index 함수가 호출됩니다. 
