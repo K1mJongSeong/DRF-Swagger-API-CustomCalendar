448cb562af55a5a18a8f6b38f3b1cc884589c116
juneyoung <jadw951623@gmail.com>
2023-03-03 12:40:32 +0900

    refactor. 쓸데 없는 주석 삭제

--- FE/src/routes/PrivateRoute.tsx
+++ FE/src/routes/PrivateRoute.tsx
@@ -9,17 +9,10 @@ interface PrivateRouteProps {
 export default function PrivateRoute({
   authentication,
 }: PrivateRouteProps): React.ReactElement | null {
-  /**
-   * 로그인 했는지 여부
-   * 로그인 했을 경우 : true 라는 텍스트 반환
-   * 로그인 안했을 경우 : null or false(로그아웃 버튼 눌렀을경우 false로 설정) 반환
-   */
   const isAuthenticated = sessionStorage.getItem('isAuthenticated');
 
   if (authentication) {
     // 인증이 반드시 필요한 페이지
-
-    // 인증을 안했을 경우 로그인 페이지로, 했을 경우 해당 페이지로
     return isAuthenticated === null || isAuthenticated === 'false' ? (
       <Navigate to="/" />
     ) : (
@@ -27,8 +20,6 @@ export default function PrivateRoute({
     );
   } else {
     // 인증이 반드시 필요 없는 페이지
-
-    // 인증을 안햇을 경우 해당 페이지로 인증을 한 상태일 경우 main페이지로
     return isAuthenticated === null || isAuthenticated === 'false' ? (
       <Outlet />
     ) : (
