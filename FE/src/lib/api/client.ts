87db3843bfd7a968fa8867f1fb9d31b778c79966
juneyoung <jadw951623@gmail.com>
2023-03-07 15:33:41 +0900

    feat. API base url 변경

--- FE/src/lib/api/client.ts
+++ FE/src/lib/api/client.ts
@@ -1,7 +1,7 @@
 import axios from 'axios';
 
 const client = axios.create();
-client.defaults.baseURL = 'http://192.168.0.158:8080/';
+client.defaults.baseURL = 'http://121.254.171.155:3003/';
 client.defaults.withCredentials = true;
 
 export default client;
