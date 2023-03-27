166425dc85e71bfa10e4e5c41d1b012c8a20ba8d
juneyoung <jadw951623@gmail.com>
2023-03-27 15:46:02 +0900

    feat: page 작업물 post API 연동

--- FE/src/containers/editor/EditorTopSection.tsx
+++ FE/src/containers/editor/EditorTopSection.tsx
@@ -1,9 +1,13 @@
+/* eslint-disable prefer-const */
 import { EditorTextButton } from 'components/editor/EditorButtons';
 import EditorTop from 'components/editor/EditorTop';
 import { useNavigate, useParams } from 'react-router';
 import { BsCartPlus } from 'react-icons/bs';
 import { MdArrowBackIos } from 'react-icons/md';
 import { useSearchParams } from 'react-router-dom';
+import { useAppDispatch, useAppSelector } from 'hooks';
+import { RootState } from 'store';
+import { postPage } from 'reducer/page';
 
 const EditorTopSection = ({
   children,
@@ -12,6 +16,9 @@ const EditorTopSection = ({
   children?: React.ReactNode;
   onSubmit?: () => void;
 }) => {
+  const dispatch = useAppDispatch();
+  const { imgs } = useAppSelector((state: RootState) => state.images);
+
   const navigate = useNavigate();
   const params = useParams();
   const [searchParams] = useSearchParams();
@@ -30,9 +37,18 @@ const EditorTopSection = ({
   };
 
   const handlePostPageClick = () => {
-    alert(
-      `템플릿 이름: ${temp}, 선택 년도: ${year}, 저장 페이지: ${page}, 페이지이름: ${pageName}`,
-    );
+    if (!pageName || !nansu) return;
+
+    let newArr: Array<string> = [];
+    imgs.forEach((el) => {
+      if (el.pageNo.toString() === page) {
+        newArr.push(el.imgUrl);
+      }
+    });
+    dispatch(postPage({ pageName, pagePayload: { pic: newArr, nansu } }));
+    // alert(
+    //   `템플릿 이름: ${temp}, 선택 년도: ${year}, 저장 페이지: ${page}, 페이지이름: ${pageName}`,
+    // );
   };
 
   const handleGotoOrder = () => {
