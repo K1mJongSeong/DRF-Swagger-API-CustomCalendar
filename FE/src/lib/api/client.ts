690fc5e752bc8c4a7f9c7d3b92fad84c37524237
juneyoung <jadw951623@gmail.com>
2023-03-28 17:11:46 +0900

    feat GET 난수 확인 API 수정

--- FE/src/lib/api/client.ts
+++ FE/src/lib/api/client.ts
@@ -1,7 +1,10 @@
 import axios from 'axios';
 
 const client = axios.create();
+
+// local
 client.defaults.baseURL = 'http://192.168.0.158:8080/';
+// server
 // client.defaults.baseURL = 'http://121.254.171.155:3003/';
 client.defaults.withCredentials = true;
 
